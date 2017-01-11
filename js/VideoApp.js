0b7d72ac6736617efcda3e6fea0328ff937e947f
Jake <jecobwesterfield@gmail.com>
2017-01-10 22:03:46 -0800

    VideoApp
    Testing a video chat application using a Real Time Computing(RTC), and
    the simplewebRTC API

---
+++ js/VideoApp.js
@@ -0,0 +1,16 @@
+function createVideoChat(videoID1,videoID2,chatRoomName){
+  var videoChat = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: videoID1,
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: videoID2,
+    // immediately ask for camera access
+    autoRequestMedia: true
+	});
+  videoChat.on('readyToCall', function () {
+    // you can name it anything
+    videoChat.joinRoom(chatRoomName);
+  });
+}
+
+createVideoChat("localVideo", "remoteVideos", "myChatRoom")
